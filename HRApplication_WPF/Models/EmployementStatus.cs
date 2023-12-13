9a94b1f4a8283916a621d7fa56dbe94e8dde4665
tomwiet <tthanquol@gmail.com>
2023-12-13 22:46:30 +0100

    in progress

---
+++ HRApplication_WPF/Models/EmployementStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRApplication_WPF.Models
+{
+  public enum EmployementStatus
+    {
+        Wszyscy = 0,
+        Zatrudnieni,
+        Zwolnieni
+
+    }
+   
+}
--- HRApplication_WPF/ViewModels/MainWindowViewModel.cs
+++ HRApplication_WPF/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using HRApplication_WPF.Models;
+using HRApplication_WPF.Views;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HRApplication_WPF.ViewModels
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : ViewModelBase
     {
+      private Repository _repository = new Repository();
+        public MainWindowViewModel()
+        {
+           
+        }
+       
+        
     }
 }
