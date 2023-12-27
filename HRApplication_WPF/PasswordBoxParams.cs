ea1eb6b813def92ae2e2079909b5e2b67692795a
tomwiet <tthanquol@gmail.com>
2023-12-27 09:23:09 +0100

    Added database structure dump file

---
+++ HRApplication_WPF/PasswordBoxParams.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace HRApplication_WPF
+{
+    public class PasswordBoxParams
+    {
+        public Window Window { get; set; }
+        public PasswordBox PasswordBox { get; set; }
+    }
+}
